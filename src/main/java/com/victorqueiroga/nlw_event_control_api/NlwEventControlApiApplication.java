f631eae03ae66b5fd36b52342954ce35683be5ff
Victor Queiroga <victorqueiroga.jp@gmail.com>
2025-02-17 22:11:17 -0300

    first commit

---
+++ src/main/java/com/victorqueiroga/nlw_event_control_api/NlwEventControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.victorqueiroga.nlw_event_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwEventControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwEventControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorqueiroga/nlw_event_control_api/NlwEventControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorqueiroga.nlw_event_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwEventControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
